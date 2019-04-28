4557d0f1d29af751ca029b476069461e229df9f6
gyb <876368840@qq.com>
2019-04-28 16:48:48 +0800

    rtet

--- src/SyncJob.Application/AppServices/RegistrationService.cs
+++ src/SyncJob.Application/AppServices/RegistrationService.cs
@@ -4,11 +4,13 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp;
 using Volo.Abp.Application.Services;
 using Volo.Abp.BackgroundJobs;
 
 namespace AppServices
 {
+    [RemoteService(IsEnabled = false)]
     public class RegistrationService : ApplicationService
     {
         private readonly IBackgroundJobManager _backgroundJobManager;
---
+++ src/SyncJob.Application/AppServices/TestService.cs
@@ -0,0 +1,22 @@
+﻿using Hangfire;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Application.Services;
+ 
+
+namespace AppServices
+{
+    [RemoteService(IsEnabled = true)]
+    public class TestService: ApplicationService
+    {
+        public void Test()
+        {
+             Console.WriteLine("Hello world!");
+        }
+
+         
+    }
+}
